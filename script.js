9a66dc5bc45160c79d4246cbb9124825adda7e20
root <root@ip-172-31-31-4.ec2.internal>
2023-11-28 21:54:53 +0000

    pushing the three files for the front end app development

---
+++ script.js
@@ -0,0 +1,10 @@
+const addProductForm = document.getElementById('addProductForm');
+const deleteProductForm = document.getElementById('deleteProductForm');
+const productsTable = document.getElementById('productsTable');
+
+addProductForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const barcode
+
+
